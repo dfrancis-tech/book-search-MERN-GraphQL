2debe23d7b2ae77676301a89760a04e0bcfd74db
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-29 17:51:06 -0400

    replace RESTapi with GraphQL schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
