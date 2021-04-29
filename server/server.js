108b3a89d44b3e3e56662c242436daaba6969e94
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-29 19:52:11 -0400

    install npm dependencies for graphql and apollo server

--- server/server.js
+++ server/server.js
@@ -8,9 +8,8 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -26,7 +25,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 //app.use(routes);
 
 db.once('open', () => {
