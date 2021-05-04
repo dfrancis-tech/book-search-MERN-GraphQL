fedae7e2bd10af24a6c01f893fbf5b6a0a4851cc
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-05-03 21:05:20 -0400

    fix typo for datatype of bookId

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
   type Book {
     authors: [String]
     description: String
-    bookId: String
+    bookId: ID!
     image: String
     link: String
     title: String
@@ -36,7 +36,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookData: BookInput!): User
-    removeBook(bookId: String!): User
+    removeBook(bookId: ID!): User
   }
 
 `;
